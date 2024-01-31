51721986ff4dc62754d5a066a2ffcdfd714020b4
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2024-01-30 18:55:21 -0700

    Added player controller

--- Assets/Scripts/Player/BasicMovement.gd
+++ Assets/Scripts/Player/BasicMovement.gd
@@ -2,9 +2,25 @@ extends CharacterBody3D;
 
 @export var speed = 5.0;
 @export var jump_velocity = 4.5;
+@export var min_look_angle = -45;
+@export var max_look_angle = 75;
+
+@onready var neck = $Neck;
+@onready var camera = $Neck/Camera3D;
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _unhandled_input(event):
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * 0.01 * 0.5);
+			camera.rotate_x(-event.relative.y * 0.01 * 0.5);
+			camera.rotation.x = clamp(
+				camera.rotation.x, 
+				deg_to_rad(min_look_angle), 
+				deg_to_rad(max_look_angle)
+			);
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,8 +33,13 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector(
+		"movement_left", 
+		"movement_right", 
+		"movement_forward", 
+		"movement_backward"
+	)
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
--- Assets/Scripts/Player/Player.gd
+++ Assets/Scripts/Player/Player.gd
@@ -1,1 +1,9 @@
 extends Node;
+
+func _unhandled_input(event):
+    # We want to capture the mouse if the player has clicked inside the game window.
+    if event is InputEventMouseButton:
+        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+    # We want to release the mouse if the player has clicked escape.
+    elif event.is_action_pressed("ui_cancel"):
+        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
